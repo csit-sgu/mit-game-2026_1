aef2dea9e78088a0b058a24de68ed7176f4fbc70
anisifor1305 <1accnextrp1@gmail.com>
2026-05-11 13:30:01 +0300

    feat(CheckFinish): implement CheckFinish function

--- user.cpp
+++ user.cpp
@@ -193,6 +193,13 @@ bool CheckPlayerDeath(Object &player, Scene &scene) {
 // Ваше решение может сильно отличаться.
 //
 bool CheckFinish(Object &player, Scene &scene) {
+    for (auto &obj : scene) {
+        if (obj.finish.enabled) {
+            if (CheckCollision(player, obj).exists) {
+                return true;
+            }
+        }
+    }
     return false;
 }
 
